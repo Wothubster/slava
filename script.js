dbcdf11b91644f22c0524884298fd64735ce4a7a
Wothubster <80402269+Wothubster@users.noreply.github.com>
2024-01-22 18:18:47 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function toggleMenu() {
+    var menu = document.querySelector('.main-nav');
+    menu.classList.toggle('show');
+}
+
+function closeCasinoDescription() {
+    document.getElementById('casino-description').style.display = 'none';
+}
+
+
+
